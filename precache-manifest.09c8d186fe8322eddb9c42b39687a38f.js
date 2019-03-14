b483f0fd543ba0d839b2edf25b0bb9395bc3bd5c
Daniel Dylla <daniel.dylla@gmail.com>
2019-03-13 22:02:57 -0500

    initializing

---
+++ precache-manifest.09c8d186fe8322eddb9c42b39687a38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08aebd195d604a1b9c7a",
+    "url": "/static/css/main.52275973.chunk.css"
+  },
+  {
+    "revision": "08aebd195d604a1b9c7a",
+    "url": "/static/js/main.233b4f14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef93aec85efd61dc5c05",
+    "url": "/static/css/2.070251e1.chunk.css"
+  },
+  {
+    "revision": "ef93aec85efd61dc5c05",
+    "url": "/static/js/2.c2bb5acb.chunk.js"
+  },
+  {
+    "revision": "77475a1d63abae184a7727c25ca07487",
+    "url": "/index.html"
+  }
+];
