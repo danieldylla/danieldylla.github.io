5c83266f2f5c9bec1ebaebdb87ede2e2034b288c
Daniel Dylla <daniel.dylla@gmail.com>
2019-04-09 21:05:36 -0500

    undos

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31626de6e6f7cbaacb8125574ef6a2a1.js"
+  "/precache-manifest.7973b3c060ed93f53cfd92feadd12f19.js"
 );
 
 workbox.clientsClaim();
