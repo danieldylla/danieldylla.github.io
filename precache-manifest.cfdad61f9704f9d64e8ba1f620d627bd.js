b2d6a0e3a574d1077c03bed12c845835757a6c70
Daniel Dylla <daniel.dylla@gmail.com>
2019-04-12 17:21:18 -0500

    removed log from firebase

---
+++ precache-manifest.cfdad61f9704f9d64e8ba1f620d627bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d799d3f19f6ceacc7b60",
+    "url": "/static/css/main.99f0a062.chunk.css"
+  },
+  {
+    "revision": "d799d3f19f6ceacc7b60",
+    "url": "/static/js/main.f9a64b39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "29cf1c43eab248d63123",
+    "url": "/static/css/2.effa9564.chunk.css"
+  },
+  {
+    "revision": "29cf1c43eab248d63123",
+    "url": "/static/js/2.ee4ade36.chunk.js"
+  },
+  {
+    "revision": "47f8bc27c2935b8f98889c9db2cb0860",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7973b3c060ed93f53cfd92feadd12f19.js"
+  "/precache-manifest.cfdad61f9704f9d64e8ba1f620d627bd.js"
 );
 
 workbox.clientsClaim();
